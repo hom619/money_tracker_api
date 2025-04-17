4e638e04723c2500f9d7fabc03b1d4daa9686d5e
hom619 <hom.grg619@gmail.com>
2025-04-17 22:40:47 +1000

    Server side setup(installation of packages and port for server)

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from "express";
+const app = express();
+const PORT = process.env.PORT || 8000;
+app.get("/", (req, res) => {
+  res.json({
+    message: "Its live",
+  });
+});
+app.listen(PORT, (error) => {
+  error
+    ? console.log(error)
+    : console.log(`Server running at http://localhost:${PORT}`);
+});
